rockbaaska/wot-xvm

commit e204a6b096349a349aca7f735937252a846e1337

localization: always init before config loaded event
git-svn-id: http://wot-xvm.googlecode.com/svn@2346 802240b0-0b4d-18ce-4811-9324b01ec0ff

trunk/src/xvm/src/com/xvm/ConfigLoader.as
@@ -18,10 +18,11 @@ class com.xvm.ConfigLoader
     // Private vars
     private static var s_loading:Boolean = false;
     private static var info_event:Object = null;
+    private static var _region_loaded = false;
 
     // instance
     private static var instance:ConfigLoader = null;
-    
+
     // Load XVM mod config; config data is shared between all marker instances, so
     // it should be loaded only once per session. s_loaded flag indicates that
     // we've already initialized config loading process.
@@ -80,6 +81,9 @@ class com.xvm.ConfigLoader
                 return;
             finallyBugWorkaround = true;
             ReloadGameRegion();
+
+            /** Load localization files */
+            GlobalEventDispatcher.addEventListener(Locale.EVENT_LOADED, ConfigLoader.instance.SetConfigLoaded);
             Locale.loadLocale();
             //Logger.add("TRACE: ReloadConfigCallback(): finally::end");
         }
@@ -161,6 +165,7 @@ class com.xvm.ConfigLoader
                 if (finallyBugWorkaround)
                     return;
                 finallyBugWorkaround = true;
+                ConfigLoader._region_loaded = true;
                 ConfigLoader.instance.SetConfigLoaded();
             }
         });
@@ -208,6 +213,7 @@ class com.xvm.ConfigLoader
                 if (finallyBugWorkaround)
                     return;
                 finallyBugWorkaround = true;
+                ConfigLoader._region_loaded = true;
                 ConfigLoader.instance.SetConfigLoaded();
             }
         };
@@ -217,9 +223,16 @@ class com.xvm.ConfigLoader
 
     private function SetConfigLoaded()
     {
-        //Logger.add("Config: Loaded");
-        Config.s_loaded = true;
-        ConfigLoader.s_loading = false;
-        GlobalEventDispatcher.dispatchEvent({type: Config.E_CONFIG_LOADED});
+        /** prevent firing config loaded event before Locale files are loaded */
+        if (Locale.isLoaded()){
+            GlobalEventDispatcher.removeEventListener(Locale.EVENT_LOADED);
+
+            if (_region_loaded) {
+                //Logger.add("Config: Loaded");
+                Config.s_loaded = true;
+                ConfigLoader.s_loading = false;
+                GlobalEventDispatcher.dispatchEvent({type: Config.E_CONFIG_LOADED});
+            }
+        }
     }
 }

trunk/src/xvm/src/com/xvm/Locale.as
@@ -1,8 +1,8 @@
-/**
+﻿/**
  * ...
  * @author Maxim Schedriviy
  * @author Michael Pavlyshko
- * @author Pavel Maca
+ * @author Pavel Máca
  */
 import com.xvm.Config;
 import com.xvm.ConfigUtils;
@@ -15,10 +15,13 @@ import net.wargaming.managers.Localization;
 
 class com.xvm.Locale
 {
+    public static var EVENT_LOADED:String = "locale_loaded";
+
+    private static var _initialized:Boolean = false;
+    private static var _language:String;
+    private static var _loaded:Boolean = false;
     private static var s_lang:Object;
     private static var s_lang_fallback:Object = {};
-    private static var _language:String;
-    private static var _initialized:Boolean = false;
     private static var timer:Number;
 
     /////////////////////////////////////////////////////////////////
@@ -45,13 +48,18 @@ class com.xvm.Locale
         return s_lang.locale[text] || s_lang_fallback[text] || text;
     }
 
+    public static function isLoaded():Boolean
+    {
+        return _loaded;
+    }
+
     /////////////////////////////////////////////////////////////////
     // PRIVATE
 
     private static function getLanguageFromGettext():Void
     {
         //Logger.add("Locale: timer tick");
-        if (ExternalInterface.available) 
+        if (ExternalInterface.available)
         {
             clearInterval(timer);
             timer = null;
@@ -84,13 +92,13 @@ class com.xvm.Locale
             tr["Chance to win"] = "Шанс на победу";
             tr["global"] = "общий";
             tr["per-vehicle"] = "по технике";
-            
+
             // Hitlog
             tr["attack"] = "атака";
             tr["fire"] = "пожар";
             tr["ramming"] = "таран";
             tr["world_collision"] = "падение";
-            
+
             // Capture
             tr["<font size='15' color='#FFFFFF'>Enemy base capture! {{extra}}</font>"] =
                 "<font size='15' color='#FFFFFF'>Захват вражеской базы! {{extra}}</font>";
@@ -102,12 +110,12 @@ class com.xvm.Locale
                 "<font size='17' color='#FFCC66'>Наша база захвачена!</font>";
             tr["Capturers: <b><font color='#FFCC66'>{{tanks}}</font></b> Timeleft: <b><font color='#FFCC66'>{{time}}</font><b>"] =
                 "Захватчиков: <b><font color='#FFCC66'>{{tanks}}</font></b> Осталось: <b><font color='#FFCC66'>{{time}}</font><b>";
-            
+
             // FinalStatistics
             tr["Hit percent"] = "Процент попаданий";
             tr["Damage upon detecting"] = "Урон по вашим разведданным";
             tr["Damage dealt"] = "Нанесенный урон";
-            
+
             // TeamRenderers
             tr["TeamRenderersHeaderTip"] =
                 "Рейтинг xwn (или xeff).\n" +
@@ -117,7 +125,7 @@ class com.xvm.Locale
             tr["Load statistics"] = "Загрузить статистику";
             tr["enabled"] = "включено";
             tr["disabled"] = "выключено";
-            
+
             // UserInfo
             tr["UserInfoEHint"] =
                 "Эффективность по танку.\n" +
@@ -134,7 +142,7 @@ class com.xvm.Locale
             tr["Defence"] = "Защита";
             tr["Capture"] = "Захват";
             tr["player (average / top)"] = "игрок (средний / топ)";
-            
+
             // UserInfo - filters
             tr["Filter"] = "Фильтр";
             tr["Spec dmg"] = "Уд. дамаг";
@@ -144,7 +152,7 @@ class com.xvm.Locale
             tr["Show all tanks played"] = "Показать все танки, на которых играл";
             tr["In hangar"] = "В ангаре";
             tr["Show only tanks in own hangar"] = "Показать только танки в своем ангаре";
-            
+
             // UserInfo - buttons
             tr["Level"] = "Уровень";
             tr["Type"] = "Тип";
@@ -152,11 +160,11 @@ class com.xvm.Locale
             tr["Name"] = "Имя";
             tr["Fights"] = "Боёв";
             tr["Wins"] = "Побед";
-            
+
             // Crew
             tr["PutOwnCrew"] = "Родной экипаж";
             tr["PutBestCrew"] = "Лучший экипаж";
-            
+
             // Squad
             tr["Vehicle"] = "Танк";
             tr["Battle tiers"] = "Уровень боёв";
@@ -176,26 +184,26 @@ class com.xvm.Locale
         {
             /** Hardcoded EN language */
             tr["XVM_translator"] = "Maxim Schedriviy";
-            
+
             // Hitlog
             tr["world_collision"] = "falling";
-            
+
             // UserInfo
             tr["UserInfoEHint"] =
                 "Per-vehicle efficiency.\n" +
                 "The values shown are as of the last statistics update: %DATE%\n" +
                 "See actual current values in the detailed vehicle info.\n" +
                 "Accuracy of the column values depends on the quality of the feed data.";
-            
+
             // TeamRenderers
             tr["TeamRenderersHeaderTip"] =
                 "Xwn (or xeff) rating.\n" +
                 "To see detailed information, move mouse cursor to the player's name.";
-            
+
             // Crew
             tr["PutOwnCrew"] = "Put own crew";
             tr["PutBestCrew"] = "Put best crew";
-            
+
             // Squad
             tr["ussr"] = "USSR";
             tr["germany"] = "Germany";
@@ -213,20 +221,22 @@ class com.xvm.Locale
             Logger.add("Locale: Loaded '" + _language + "' language by " + get("XVM_translator"));
         }
         else {
-            if (event.error.type == "NO_FILE")
+            if (event.error.type != "NO_FILE")
             {
+                var ex = event.error;
+                var text:String = "Error loading language file '" + event.filename + "': ";
+                text += ConfigUtils.parseErrorEvent(event);
+
+                /** Show error message on battle loading */
+                var info_event = { type: Config.E_SET_INFO, error: text };
+                GlobalEventDispatcher.dispatchEvent(info_event);
+                Logger.add(String(text).substr(0, 200));
+            }else{
                 Logger.add("Locale: Can not find language file. Filename: " + event.filename );
-                return;
             }
-
-            var ex = event.error;
-            var text:String = "Error loading language file '" + event.filename + "': ";
-            text += ConfigUtils.parseErrorEvent(event);
-
-            /** Show error message on battle loading */
-            var info_event = { type: "set_info", error: text };
-            GlobalEventDispatcher.dispatchEvent(info_event);
-            Logger.add(String(text).substr(0, 200));
         }
+
+        _loaded = true;
+        GlobalEventDispatcher.dispatchEvent( { type: EVENT_LOADED } );
     }
 }

commit 113a7931cc3b8644b561ff311a183b71bc8ec499

fix displaying error message in TipField
broken by r2346

trunk/src/xvm/src/com/xvm/ConfigLoader.as
@@ -229,6 +229,7 @@ class com.xvm.ConfigLoader
 
             if (_region_loaded) {
                 //Logger.add("Config: Loaded");
+                GlobalEventDispatcher.dispatchEvent(ConfigLoader.info_event);
                 Config.s_loaded = true;
                 ConfigLoader.s_loading = false;
                 GlobalEventDispatcher.dispatchEvent({type: Config.E_CONFIG_LOADED});

trunk/src/xvm/src/com/xvm/StatLoader.as
@@ -8,7 +8,6 @@ import com.xvm.Macros;
 import com.xvm.StatData;
 import com.xvm.Utils;
 import com.xvm.VehicleInfo;
-import com.xvm.DataTypes.Stat;
 
 class com.xvm.StatLoader
 {
@@ -69,7 +68,7 @@ class com.xvm.StatLoader
             vehicleKey: VehicleInfo.getInfo2(icon).name.toUpperCase(),
             icon: icon,
             team: team,
-      	    vehicleId: vehId,
+            vehicleId: vehId,
             vehicleState: vehicleState,
             selected: data.himself,
             loadstate: !StatData.s_data[pname].loadstate ? Defines.LOADSTATE_NONE : StatData.s_data[pname].loadstate,
@@ -139,7 +138,7 @@ class com.xvm.StatLoader
             var response = JSONx.parse(event.str);
 
             if (response.info && response.info.xvm)
-                GlobalEventDispatcher.dispatchEvent({ type: "set_info", ver: response.info.xvm.ver, message: response.info.xvm.message });
+                GlobalEventDispatcher.dispatchEvent({ type: Config.E_SET_INFO, ver: response.info.xvm.ver, message: response.info.xvm.message });
 
             if (response.players)
             {

trunk/src/xvm/src/wot/battleloading/TipField.as
@@ -1,3 +1,4 @@
+import com.xvm.Config;
 import com.xvm.Defines;
 import com.xvm.GlobalEventDispatcher;
 import com.xvm.Utils;
@@ -9,7 +10,7 @@ class wot.battleloading.TipField
     public function TipField(form_mc:MovieClip)
     {
         this.form_mc = form_mc;
-        GlobalEventDispatcher.addEventListener("set_info", this, setInfoFieldData);
+        GlobalEventDispatcher.addEventListener(Config.E_SET_INFO, this, setInfoFieldData);
         setInfoFieldData( { } );
     }
 